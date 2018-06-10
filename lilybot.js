0d22b710d78130331302a643bd2a64f4a6dca340
Lilianna <lilianna@swansontech.net>
2018-06-10 17:00:51 -0400

    added some functions

--- lilybot.js
+++ lilybot.js
@@ -1,13 +1,38 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+var serverStates = [];
+
+//declare shortcut functions
+function loop(m,n){
+  for(i=0;i<n;i++){ 
+    message.channel.sendMessage(m);
+  }
+}
+function send(m){
+  // where "m" is the message to send
+  message.channel.sendMessage(m);
+}
+function tts(m){
+  // where "m" is the message to send
+  message.channel.sendTTSMessage(m);
+}
+function regex(m,p){
+  // where "m" is the subject, "p" is the pattern
+  message.channel.sendMessage(m.match(eval(p)));
+}
+
 client.on("ready", () => {
   console.log("I am ready!");
+  message.channel.send("lilybot 2.0 online!");
 });
 
 client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
     message.channel.send("pong!");
+  }
+  else if (message.content.startsWith("showguilds")){
+    message.channel.send(""+client.guilds.toString());
   }
 });
 
